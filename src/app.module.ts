befde654e89fc3255923952c8e1b1042305b8c9d
michael19 <rekmixa@gmail.com>
2023-03-21 01:03:33 +0500

    add scheduler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Logger, Module, OnApplicationBootstrap } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { TelegramModule } from './telegram/telegram.module'
-import { OpenAiService } from './telegram/open-ai.service'
 import { DbModule } from './db/db.module'
+import { TasksModule } from './tasks/tasks.module'
+import { OpenAiService } from './telegram/open-ai.service'
+import { TelegramModule } from './telegram/telegram.module'
 
 @Module({
-  imports: [ConfigModule.forRoot(), TelegramModule, DbModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TelegramModule,
+    DbModule,
+    TasksModule,
+  ],
   providers: [OpenAiService],
 })
 export class AppModule implements OnApplicationBootstrap {
