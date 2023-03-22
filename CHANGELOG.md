4f7dfa1da6e4a578f9854693bc6c31c238c7c191
michael19 <rekmixa@gmail.com>
2023-03-23 00:10:19 +0500

    change logger: add sending errors to telegram

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,13 @@
 
 ## [Unreleased]
 
-## [0.0.1] - 2023-03-19
+## [0.0.2] - 2023-03-23
+### Changed
+- logger: add sending errors to telegram
+### Fixed
+- cron schedule for send posts task
+
+## [0.0.1] - 2023-03-22
 ### Added
 - Initialize Nest JS project with Telegram API & OpenAI
 - Add logic for generating, moderating and publishing posts
--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,6 +1,7 @@
 import { ConsoleLogger } from '@nestjs/common'
 import { clc } from '@nestjs/common/utils/cli-colors.util'
 import { bytesToSize, showUptime } from 'src/helpers'
+import { sendMessageToAdmin } from 'src/telegram/telegram.module'
 
 declare type messageColor = 'green' | 'red' | 'yellow' | 'magentaBright' | 'cyanBright'
 
@@ -11,6 +12,11 @@ export default class Logger extends ConsoleLogger {
 
   error(message: any, trace?: string, context?: string) {
     super.error(this.prepareLogMessage(message, 'red'), trace, context)
+
+    sendMessageToAdmin(`${message}, ${trace}`)
+      .catch((error) => {
+        super.error(this.prepareLogMessage('Error while reporting log to telegram', 'red'), error)
+      })
   }
 
   warn(message: any, context?: string) {
